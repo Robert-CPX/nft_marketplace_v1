85c14783bfdd140f36f9a721bac5f201ee265d4e
Robert-CPX <cpx@robbdev.xyz>
2023-07-31 14:49:27 +0800

    setup commonly used components

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
-import './globals.css';
-
-export const metadata = {
-  title: 'Create Next App',
-  description: 'Welcome to Next.js',
-};
+import Script from 'next/script';
+import '../styles/globals.css';
+import { Navbar, Footer } from './components';
+import { Providers } from './providers';
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <body className="text-3xl font-bold underline">
-      {children}
+    <body>
+      <Providers>
+        <div className="dark:bg-nft-dark bg-white min-h-screen">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
+        <Script src="https://kit.fontawesome.com/7d23f8c160.js" crossOrigin="anonymous" />
+      </Providers>
     </body>
   </html>
 );
