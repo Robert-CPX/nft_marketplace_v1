7635ee79929408f17e730d914626401d88c64d03
Robert-CPX <cpx@robbdev.xyz>
2023-08-05 06:53:35 +0800

    consider refactoring with Moralis

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
-
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config({ path: '.env' });
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: '0.8.19',
+  networks: {
+    // The name `sepolia` has no relevance here - you can name it whatever you want
+    sepolia: {
+      url: process.env.QUICKNODE_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,18 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const NFTMarketplace = await hre.ethers.deployContract('NFTMarketplace');
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await NFTMarketplace.waitForDeployment();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  console.log(`NFTMarketplace contract deployed to ${NFTMarketplace.target}`);
+
+  await sleep(45000);
+
+  await hre.run('verfiy:verify', {
+    address: NFTMarketplace.target,
+    constructorArguments: [],
   });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
